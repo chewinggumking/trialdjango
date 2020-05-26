40ee7d1875d77e33b4055021976bbe65b29e55d2
chewinggumkingm <chewinggumking@gmail.com>
2020-05-27 00:19:47 +0530

    pseudocode for parking and interest.

--- billing/utilities.py
+++ billing/utilities.py
@@ -17,3 +17,20 @@ def create_flat_bills(flat, bill_type):
         elec_ins_chgs = bill_type.elec_ins_chgs,
         noc_charges = rental_noc,
     )
+
+    """ 
+    PARKING CHARGES
+    If flat has vehicle, check the type of vehicle
+        then check the type of parking, 
+        assign it to a variable, 
+         If flat has more vehicles and what kind of vehicle code
+        add additional cost to parking charges.
+    
+    Interest on non payment
+    If flat has past bills that are unpaid(is_paid = False)
+        in the next bill add last bill + 21%
+    in the next bill if the flat still has unpaid bills
+        take the amount of last bill without interest
+        add it the present bill amount and add interest on both the 
+        amounts for a new total
+    """
